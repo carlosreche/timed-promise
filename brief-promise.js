1c1877f9a6349bb8c48d017a3f6768c516b3ea4b
carlosreche <48285411+carlosreche@users.noreply.github.com>
2020-11-16 16:10:28 -0300

    Update brief-promise.js

--- brief-promise.js
+++ brief-promise.js
@@ -28,54 +28,11 @@
  * seen implementations using Promise.race() to cancel the unresolved Promise,
  * skipping this step and hopefuly making the script more efficient.
  * 
+ * @author Carlos Henrique Reche
+ * 
  * 
  * @param {Function} executor the asyncronous code to be performed
  * @param {integer} milliseconds time to wait before rejecting the Promise
- * 
- * @example
- *
- *    const BriefPromise = require("path/to/brief-promise.js");
- *    
- *    const executor = (resolve, reject) => {
- *      // some task to be executed asynchronously resulting in a call to "resolve" or "reject"
- *      setTimeout(resolve, 2000, 'value encountered after 2 seconds');
- *    };
- *    
- *    new BriefPromise(executor)
- *      .timeout(5000) // five seconds to complete the executor (a call to "resolve" or "reject")
- *      .then(
- *        (value) => {
- *          console.log(`Successful Response:\n  ${value}`);
- *          // do another task and return some new value
- *          const newValue = new Promise(
- *            (resolve) => {
- *              setTimeout(resolve, 2500, 'some new value after 2.5 seconds');
- *            }
- *          );
- *          return newValue;
- *        },
- *        null, // second parameter of "then" is reserved by the Promise class (a shortcut to "catch", if not null)
- *        3000  // sets a timeout of 3 seconds to complete this "then" statement
- *      )
- *      .then(
- *        (value) => {
- *          console.log(`New Successful Response:\n  ${value}`);
- *        }
- *      )
- *      .catch(
- *        (error) => {
- *          // checking if it's a timeout error
- *          if (error instanceof BriefPromise.TimeoutError) {
- *            console.error(`The Promise timed out before finishing the task. Details:\n  ${error}`);
- *          } else {
- *            console.error(error); // other errors
- *          }
- *        }
- *      );
- * 
- * 
- * @author Carlos Henrique Reche
- * 
  */
 class BriefPromise extends Promise {
 
